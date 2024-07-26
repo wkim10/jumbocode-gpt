3563b5cb2104b89b03ccc62f3eacbd881139fb79
wkim10 <wkim10@tufts.edu>
2024-07-25 19:55:35 -1000

    auth fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaClient, User } from "@prisma/client";
-import bcrypt from "bcrypt";
+import NextAuth from "next-auth";
 
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.username || !credentials?.password) {
-          return null;
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { username: credentials.username },
-        });
-
-        if (
-          user &&
-          (await bcrypt.compare(credentials.password, user.password))
-        ) {
-          return {
-            id: user.id,
-            username: user.username,
-          };
-        } else {
-          return null;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/signin",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      console.log({ token, user });
-      if (user) {
-        token.id = user.id;
-        token.username = user.username;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      console.log({ session, token });
-      session.user = {
-        id: token.id as string,
-        username: token.username as string,
-      };
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
 
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,60 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.username || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { username: credentials.username },
+        });
+
+        if (
+          user &&
+          (await bcrypt.compare(credentials.password, user.password))
+        ) {
+          return {
+            id: user.id,
+            username: user.username,
+          };
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/signin",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log({ token, user });
+      if (user) {
+        token.id = user.id;
+        token.username = user.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      console.log({ session, token });
+      session.user = {
+        id: token.id as string,
+        username: token.username as string,
+      };
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
